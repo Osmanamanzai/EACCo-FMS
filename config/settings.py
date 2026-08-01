d0a1807af0b8e9176587a9654505d16aace04e2f
EACCo <EACCo@gmail.com>
2026-08-01 18:06:49 +0430

    remove cloudanary

--- config/settings.py
+++ config/settings.py
@@ -36,8 +36,6 @@ INSTALLED_APPS = [
     'projects',
     'transactions',
     'reports',
-    'cloudinary',
-    'cloudinary_storage',
     'django.contrib.humanize',
 ]
 
@@ -124,9 +122,3 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
-}
